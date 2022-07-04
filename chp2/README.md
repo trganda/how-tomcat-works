fa012f544b2b7ea3798ac73c7b7d65f16f4422fc
trganda <jongsx1203@gmail.com>
2022-07-04 19:39:11 +0800

    add chp2 application2

--- chp2/README.md
+++ chp2/README.md
@@ -11,6 +11,8 @@
 
 ### 使用方式
 
+运行`HttpServer`
+
 对于静态资源，放置在当前项目的`webroot`目录下，访问
 
 ```http
@@ -24,3 +26,33 @@ http://127.0.0.1:8080/servlet/com.trganda.servlet.PrimitiveServlet
 ```
 
 注意需要输入全限定名称。
+
+## Application2
+
+作者在书中提到，`Application1`的`ServletProcessor#process`方法存在一定的安全隐患。
+
+```java
+    // ...
+
+    Servlet servlet = (Servlet) tServlet.newInstance();
+    servlet.service(request, response);
+    
+    // ...
+```
+
+当调用`service`方法时，传入的`request`和`response`方法的类型分别为自己实现的`Request`和`Response`，但需要的参数类型是`ServletRequest`和`ServletResponse`。
+此时，`servlet`的开发者可以在编写`servlet`时，将参数强行向下转换为`Request`，并可以执行预期之外的`public`方法。
+
+为此，需要使用`Facade`的类，它会代理`Request|Response`从`ServletRequest|ServletResponse`实现的方法，但不提供其它方法。同时将该类型的参数传递给`service`方法。
+
+```java
+    // ...
+
+    Servlet servlet = (Servlet) tServlet.newInstance();
+
+    RequestFacade requestFacade = new RequestFacade(request);
+    ResponseFacade responseFacade = new ResponseFacade(response);
+    servlet.service(requestFacade, responseFacade);
+    
+    // ...
+```
---
+++ chp2/application2/src/main/java/com/trganda/ServletProcessor.java
@@ -0,0 +1,42 @@
+package com.trganda;
+
+import com.trganda.servlet.Request;
+import com.trganda.servlet.RequestFacade;
+import com.trganda.servlet.Response;
+import com.trganda.servlet.ResponseFacade;
+
+import javax.servlet.Servlet;
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLStreamHandler;
+
+public class ServletProcessor {
+
+    public void process(Request request, Response response) {
+        String uri = request.getUri();
+        String servletName = uri.substring(uri.lastIndexOf("/") + 1);
+
+        try {
+            URL[] urls = new URL[1];
+            URLStreamHandler streamHandler = null;
+            File classPath = new File(HttpServer.WEB_ROOT);
+            String repository =
+                    (new URL("file", null, classPath.getCanonicalPath() +
+                            File.separator)).toString();
+            urls[0] = new URL(null, repository, streamHandler);
+
+            URLClassLoader loader = new URLClassLoader(urls);
+
+            Class tServlet = loader.loadClass(servletName);
+            Servlet servlet = (Servlet) tServlet.newInstance();
+
+            RequestFacade requestFacade = new RequestFacade(request);
+            ResponseFacade responseFacade = new ResponseFacade(response);
+            servlet.service(requestFacade, responseFacade);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
