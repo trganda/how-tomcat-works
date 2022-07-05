93996dec23c4cdc8b2843b7b9b8561a53733aa57
trganda <jongsx1203@gmail.com>
2022-07-05 17:30:30 +0800

    add code

---
+++ src/test/java/UseRequestUtilExample.java
@@ -0,0 +1,17 @@
+import org.apache.catalina.util.RequestUtil;
+
+import javax.servlet.http.Cookie;
+
+public class UseRequestUtilExample {
+  public static void main(String[] args) {
+    Cookie cookie = new Cookie("user", "cindy");
+    cookie.setDomain("brainysoftware.com");
+    cookie.setPath("/commerce");
+    cookie.setMaxAge(600); // 10 minutes
+    cookie.setComment("This is an authorized user.");
+
+    String encodedCookie = RequestUtil.encodeCookie(cookie);
+    System.out.println(encodedCookie);
+
+  }
+}
